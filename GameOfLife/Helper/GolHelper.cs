49314fe1583e13046169dc6e8e5f8551dc60fe4b
ykSneerG <post@heikopieper.de>
2021-08-27 20:27:47 +0200

    Rename File

---
+++ GameOfLife/Helper/GolHelper.cs
@@ -0,0 +1,20 @@
+﻿namespace GameOfLife
+{
+
+    public enum Status { Life, Dead, Surround }
+
+
+    public struct Coord2D
+    {
+        public byte X;
+
+        public byte Y;
+
+        public Coord2D(byte x, byte y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+
+}
